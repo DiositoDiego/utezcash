4d122a5d7c0ec553382b7f723bebd48b3224e164
DiositoDiego <20213tn096@utez.edu.mx>
2023-02-10 13:45:35 -0600

    para panochas

--- src/components/btnCalculate.js
+++ src/components/btnCalculate.js
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
+import colors from '../utils/colors';
 
 export default function btnCalculate() {
   return (
-    <View>
-      <Text>btnCalculate</Text>
+    <View style={styles.viewCalculate}>
+      <TouchableOpacity style={styles.touchable}>
+        <Text style={styles.btnTitle}>Calcular</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  btnTitle:{
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  touchable:{
+    padding: 15,
+    borderRadius: 18,
+    width: '80%',
+    backgroundColor: colors.C_PRIMARIO_OSCURO,
+  },
+
+  viewCalculate:{
+    height: 100,
+    backgroundColor: colors.C_PRIMARIO,
+    borderTopRightRadius: 30,
+  }
+});
